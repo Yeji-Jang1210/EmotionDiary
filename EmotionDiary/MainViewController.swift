7bb093303ac942cfd5d7ffa5b326d1c5ba3eec3d
Yeji-Jang1210 <jyg8727@gmail.com>
2024-05-22 17:57:11 +0900

    UserDefaults에 count 저장 구현

--- EmotionDiary/MainViewController.swift
+++ EmotionDiary/MainViewController.swift
@@ -13,17 +13,8 @@ class MainViewController: UIViewController {
     @IBOutlet var emotionButtons: [UIButton]!
     @IBOutlet var emotionLabels: [UILabel]!
     
-    var emotions: [Emotion] = [
-        Emotion(name: "행복해", image: UIImage(named: "slime1")),
-        Emotion(name: "사랑해", image: UIImage(named: "slime2")),
-        Emotion(name: "좋아해", image: UIImage(named: "slime3")),
-        Emotion(name: "당황해", image: UIImage(named: "slime4")),
-        Emotion(name: "속상해", image: UIImage(named: "slime5")),
-        Emotion(name: "우울해", image: UIImage(named: "slime6")),
-        Emotion(name: "긴장해", image: UIImage(named: "slime7")),
-        Emotion(name: "지루해", image: UIImage(named: "slime8")),
-        Emotion(name: "억울해", image: UIImage(named: "slime9"))
-    ]
+    let emotions = Emotions.allCases
+    var counts: [Int] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,24 +33,30 @@ class MainViewController: UIViewController {
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "line.3.horizontal"), style: .plain, target: self, action: nil)
         self.navigationItem.title = "감정 다이어리"
         
+        if let data = UserDefaults.standard.array(forKey: "counts") as? [Int] {
+            counts = data
+        } else {
+            counts = Array(repeating: 0, count: 9)
+        }
+        
         //buttons, labels
         for index in emotionButtons.indices {
-            setEmotionUI(button: emotionButtons[index], label: emotionLabels[index], emotion: emotions[index])
+            setEmotionUI(button: emotionButtons[index], label: emotionLabels[index], emotion: emotions[index], count: counts[index])
         }
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         //클릭 횟수 올리기
-        emotions[sender.tag].count += 1
-        
-        //랜덤으로 숫자 보여지기
-        //emotions[sender.tag].count = Int.random(in: 0...100)
-        emotionLabels[sender.tag].text = emotions[sender.tag].tapResult
+        counts[sender.tag] += 1
+        UserDefaults.standard.setValue(counts, forKey: "counts")
+ 
+        emotionLabels[sender.tag].text = "\(emotions[sender.tag].title) \(counts[sender.tag])"
     }
     
-    private func setEmotionUI(button: UIButton, label: UILabel, emotion: Emotion){
+    private func setEmotionUI(button: UIButton, label: UILabel, emotion: Emotions, count: Int){
         button.setImage(emotion.image, for: .normal)
-        label.text = emotion.tapResult
+        // "행복해 9"
+        label.text = "\(emotion.title) \(count)"
         label.textAlignment = .center
         label.font = .systemFont(ofSize: 13)
     }
--- EmotionDiary/model/Emotions.swift
+++ EmotionDiary/model/Emotions.swift
@@ -11,10 +11,68 @@ import UIKit
 struct Emotion {
     var name: String
     var image: UIImage?
-    var count: Int = 0
-    var tapResult: String {
-        get {
-            return "\(name) \(count == 0 ? "" : "\(count)")"
+//    var count: Int = 0
+//    var tapResult: String {
+//        get {
+//            return "\(name) \(count == 0 ? "" : "\(count)")"
+//        }
+//    }
+}
+
+enum Emotions: CaseIterable {
+    case happy
+    case love
+    case like
+    case embarrassed
+    case upset
+    case blue
+    case bored
+    case nervous
+    case unfair
+    
+    var title: String {
+        switch self {
+        case .happy:
+            return "행복해"
+        case .love:
+            return "사랑해"
+        case .like:
+            return "좋아해"
+        case .embarrassed:
+            return "당황해"
+        case .upset:
+            return "속상해"
+        case .blue:
+            return "우울해"
+        case .bored:
+            return "지루해"
+        case .nervous:
+            return "긴장해"
+        case .unfair:
+            return "억울해"
+        }
+    }
+    
+    var image: UIImage? {
+        switch self {
+        case .happy:
+            return UIImage(named: "slime1")
+        case .love:
+            return UIImage(named: "slime2")
+        case .like:
+            return UIImage(named: "slime3")
+        case .embarrassed:
+            return UIImage(named: "slime4")
+        case .upset:
+            return UIImage(named: "slime5")
+        case .blue:
+            return UIImage(named: "slime6")
+        case .bored:
+            return UIImage(named: "slime7")
+        case .nervous:
+            return UIImage(named: "slime8")
+        case .unfair:
+            return UIImage(named: "slime9")
         }
     }
 }
